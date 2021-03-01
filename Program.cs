ab2d3de7e96866800eeaca3e504a6c37a5296e4f
TNviola <tnviola.rambler.ru>
2021-03-01 22:52:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson6
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      double a = 2;
+      double b = 4;
+      double total = a * b;
+      Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+      Console.WriteLine("Result!");
+      Console.ReadLine();
+    }
+  }
+}
